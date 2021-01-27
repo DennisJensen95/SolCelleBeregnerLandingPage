e9eae2aab9e6c1f7d0f9d00e59558e655f3a67e3
Dennis <ngh1adj95@hotmail.com>
2021-01-27 22:11:36 +0100

    Making login page

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,26 +1,63 @@
 import React, { Component, useState} from "react";
+import './login.css'
 
 export class Login extends Component {
-  
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+        sideActive: "sign-container"
+    };
+    
+    this.changeToSignIn = this.changeToSignIn.bind(this);
+    this.changeToSignUp = this.changeToSignUp.bind(this);
+  }
 
+  changeToSignUp() {
+    this.setState({sideActive: "sign-container right-panel-active"})
+  }
+
+  changeToSignIn() {
+    this.setState({sideActive: "sign-container"})
+  }
+
+  render () {
     return(
-      <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form>
-        <label>
-          <p>Username</p>
-          <input type="text" />
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password"/>
-        </label>
-        <div>
-          <button type="submit">Submit</button>
+      <body class="sign-body">
+        <div class={this.state.sideActive} id="container">
+          <div class="sign-form-container sign-up-container">
+            <form class="sign-form" action="#">
+              <span class="sign-span">Indtast din email for at oprette en konto</span>
+              <input class="sign-input" type="text" placeholder="Name" />
+              <input class="sign-input" type="email" placeholder="Email" />
+              <input class="sign-input" type="password" placeholder="Password" />
+              <button class="sign-button">Tilmeld</button>
+            </form>
+          </div>
+          <div class="sign-form-container sign-in-container">
+            <form class="sign-form" action="#">
+              <h1>Login</h1>
+              <input class="sign-input" type="email" placeholder="Email" />
+              <input class="sign-input" type="password" placeholder="Password" />
+              <a href="#">Glemt kodeord?</a>
+              <button class="sign-button">Login</button>
+            </form>
+          </div>
+          <div class="overlay-container">
+            <div class="sign-overlay">
+              <div class="overlay-panel overlay-left">
+                <h1>Velkommen tilbage</h1>
+                <p></p>
+                <button onClick={this.changeToSignIn} class="ghost" id="signIn">Login</button>
+              </div>
+              <div class="overlay-panel overlay-right">
+                <h1>Opret konto</h1>
+                <p></p>
+                <button onClick={this.changeToSignUp} class="ghost sign-button" id="signUp">Tilmeld</button>
+              </div>
+            </div>
+          </div>
         </div>
-      </form>
-    </div>
+    </body>
   );
   }
 }
