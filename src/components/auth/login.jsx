b5af1ce4999ee502fb322456aeeb0584e7bf087f
Dennis <ngh1adj95@hotmail.com>
2021-02-07 18:56:01 +0100

    Login page database implemented

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, setError } from "react";
+import { auth, generateUserDocument } from "../firebase";
 import './login.css'
 
 export class Login extends Component {
@@ -6,21 +7,46 @@ export class Login extends Component {
     super(props);
     this.state = {
       sideActive: "sign-container",
+      buttonState: "sign-button",
       email: "",
       password: "",
-      buttonState: "sign-button"
+      displayName: "",
+      errorMessage: "",
     };
 
     this.changeToSignIn = this.changeToSignIn.bind(this);
     this.changeToSignUp = this.changeToSignUp.bind(this);
   }
 
+  createUserWithEmailAndPasswordHandler = async (event) => {
+    event.preventDefault();
+
+    console.log("Email: %s, Password: %s", this.state.email, this.state.password);
+    await auth.createUserWithEmailAndPassword(this.state.email, this.state.password).then((result) => {
+      console.log("User %s", result.user);
+      generateUserDocument(result.user, this.state.displayName);
+    }).catch((error) => {
+      this.setState({ errorMessage: error.message })
+    });
+
+    this.setState({ email: "", password: "", displayName: "", errorMessage: "" });
+    this.changeToSignIn();
+  };
+
+  signInWithEmailAndPasswordHandler = (event) => {
+    event.preventDefault();
+    auth.signInWithEmailAndPassword(this.state.email, this.state.password).catch(error => {
+      setError("Error signing in with password and email!");
+      console.error("Error signing in with password and email", error);
+    });
+  };
+
   setEmail(email) {
-    this.setState({ email: email });
+    this.setState({ email: email, errorMessage: "" });
   }
 
   setPassword(password) {
-    this.setState({ password });
+    this.setState({ password: password, errorMessage: "" });
   }
 
   changeToSignUp() {
@@ -42,38 +68,41 @@ export class Login extends Component {
 
   render() {
     return (
-      <div class="sign-body">
-        <div class={this.state.sideActive} id="container">
-          <div class="sign-form-container sign-up-container">
-            <form class="sign-form" action="#">
-              <h1 class="sign-h1">Opret konto</h1>
-              <span class="sign-span">Indtast din email for at oprette en konto</span>
-              <input class="sign-input" type="text" placeholder="Name" />
-              <input onChange={(e) => this.setEmail(e.target.value)} class="sign-input" type="email" placeholder="Email" />
-              <input onChange={(e) => this.setPassword(e.target.value)} class="sign-input" type="password" placeholder="Password" />
-              <button class="sign-button" disabled={!this.validateForm()}>Tilmeld</button>
+      <div className="sign-body">
+        <div className={this.state.sideActive} id="container">
+          <div className="sign-form-container sign-up-container">
+            <form className="sign-form" action="#">
+              <h1 className="sign-h1">Opret konto</h1>
+              <span className="sign-span">Indtast din email for at oprette en konto</span>
+              <input className="sign-input" type="text" placeholder="Name" />
+              <input onChange={(e) => this.setEmail(e.target.value)} className="sign-input" type="email" placeholder="Email" />
+              <input onChange={(e) => this.setPassword(e.target.value)} className="sign-input" type="password" placeholder="Password" />
+              <p className="sign-p-error">{this.state.errorMessage}</p>
+              <button className="sign-button" onClick={event => {
+                this.createUserWithEmailAndPasswordHandler(event);
+              }} disabled={!this.validateForm()} >Tilmeld</button>
             </form>
           </div>
-          <div class="sign-form-container sign-in-container">
-            <form class="sign-form" action="#">
-              <h1 class="sign-h1">Login</h1>
-              <input onChange={(e) => this.setEmail(e.target.value)} class="sign-input" type="email" placeholder="Email" />
-              <input onChange={(e) => this.setPassword(e.target.value)} class="sign-input" type="password" placeholder="Password" />
+          <div className="sign-form-container sign-in-container">
+            <form className="sign-form" action="#">
+              <h1 className="sign-h1">Login</h1>
+              <input onChange={(e) => this.setEmail(e.target.value)} className="sign-input" type="email" placeholder="Email" />
+              <input onChange={(e) => this.setPassword(e.target.value)} className="sign-input" type="password" placeholder="Password" />
               <a href="/">Glemt kodeord?</a>
-              <button class="sign-button" disabled={!this.validateForm()}>Login</button>
+              <button className="sign-button" onClick={(event) => { this.signInWithEmailAndPasswordHandler(event) }} disabled={!this.validateForm()}>Login</button>
             </form>
           </div>
-          <div class="overlay-container">
-            <div class="sign-overlay">
-              <div class="overlay-panel overlay-left">
-                <h1 class="sign-h1">Velkommen tilbage</h1>
-                <p class="sign-p">Se dine udførte beregninger igen</p>
-                <button onClick={this.changeToSignIn} class="ghost sign-button" id="signIn">Login</button>
+          <div className="overlay-container">
+            <div className="sign-overlay">
+              <div className="overlay-panel overlay-left">
+                <h1 className="sign-h1">Velkommen tilbage</h1>
+                <p className="sign-p">Se dine udførte beregninger igen</p>
+                <button onClick={this.changeToSignIn} className="ghost sign-button" id="signIn">Login</button>
               </div>
-              <div class="overlay-panel overlay-right">
-                <h1 class="sign-h1">Opret konto</h1>
-                <p class="sign-p">Få adgang til solcelle beregneren</p>
-                <button onClick={this.changeToSignUp} class="ghost sign-button" id="signUp">Tilmeld</button>
+              <div className="overlay-panel overlay-right">
+                <h1 className="sign-h1">Opret konto</h1>
+                <p className="sign-p">Få adgang til solcelle beregneren</p>
+                <button onClick={this.changeToSignUp} className="ghost sign-button" id="signUp">Tilmeld</button>
               </div>
             </div>
           </div>
--- src/components/firebase.jsx
+++ src/components/firebase.jsx
@@ -1,7 +1,7 @@
 import firebase from "firebase/app";
+import "firebase/analytics"
 import "firebase/auth";
 import "firebase/firestore";
-import { functions } from "firebase";
 
 var firebaseConfig = {
   apiKey: "AIzaSyCytItS9uVIXejdDNKc4V8VcRyYcflvxxo",
@@ -23,3 +23,36 @@ const provider = new firebase.auth.GoogleAuthProvider();
 export const signInWithGoogle = () => {
   auth.signInWithPopup(provider);
 };
+
+export const generateUserDocument = async (user, additionalData) => {
+  if (!user) return;
+  const userRef = firestore.doc(`users/${user.uid}`);
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { email, displayName } = user;
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        ...additionalData
+      });
+    } catch (error) {
+      console.error("Error creating user document", error);
+    }
+  }
+  return getUserDocument(user.uid);
+};
+
+const getUserDocument = async uid => {
+  if (!uid) return null;
+  try {
+    const userDocument = await firestore.doc(`users/${uid}`).get();
+    return {
+      uid,
+      ...userDocument.data()
+    };
+  } catch (error) {
+    console.error("Error fetching user", error);
+  }
+};
+
