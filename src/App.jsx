e132ead523c4d44571281c4c63d7dd708d710200
Dennis <ngh1adj95@hotmail.com>
2021-02-24 22:08:59 +0100

    Parking the page here

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 import Navigation from './components/navigation';
 import Header from './components/header';
-import Beregner from './components/beregner';
-import About from './components/about';
-import Services from './components/services';
 import JsonData from './data/data.json';
 import EnergyConsumption from "./components/energy_consumption_dk"
 
@@ -12,7 +9,7 @@ export class App extends Component {
     landingPageData: {},
   }
   getlandingPageData() {
-    this.setState({landingPageData : JsonData})
+    this.setState({ landingPageData: JsonData })
   }
 
   componentDidMount() {
@@ -25,9 +22,6 @@ export class App extends Component {
         <Navigation />
         <Header data={this.state.landingPageData.Header} />
         <EnergyConsumption data={this.state.landingPageData.Energi} />
-        <About data={this.state.landingPageData.About} />
-        <Services data={this.state.landingPageData.Services} />
-        <Beregner data={this.state.landingPageData.Beregner} />
       </div>
     )
   }
